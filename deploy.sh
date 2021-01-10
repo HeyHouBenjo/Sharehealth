5ce45b7dae0875d5c4417c2a1ac0e43c7644bee5
Benjamin Kraft <benjamin.kraft@online.de>
2021-01-10 14:55:23 +0100

    updated path in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-cp IntelliJ_IDEA/Sharehealth_1.16.1/target/Sharehealth.jar Minecraft_Servers/Server_1.16.1/plugins/
+cp IntelliJ_IDEA/Sharehealth/target/Sharehealth.jar Minecraft_Servers/Server_1.16.1/plugins/
