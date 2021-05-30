e71bcc02440d11df4e0418083ddf771d934c1368
Benjo <benjamin.kraft@online.de>
2021-05-30 02:56:38 +0200

    transfer

--- scripts/start_server.bat
+++ scripts/start_server.bat
@@ -1,2 +1,3 @@
 cd server
 java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar spigot.jar nogui
+ https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
