b53ad62cd68d11e4dbd9084e3d0f7142c037298a
Default <default@mail.com>
2022-03-01 20:56:17 +0100

    start_server TODO

--- scripts/start_server.py
+++ scripts/start_server.py
@@ -1,3 +1,10 @@
 import os
+import subprocess
 
-os.system("cd server && java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar paper.jar nogui")
+p = subprocess.Popen(
+    "java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar paper.jar nogui",
+    cwd=os.getcwd() + "/server", shell=True, stdin=subprocess.PIPE
+)
+
+
+#os.system("cd server && java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar paper.jar nogui")
