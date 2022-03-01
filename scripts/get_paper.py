da3f10fddd29a3f161ba0006aa830ea9c921877e
Benjamin <benjamin.kraft@online.de>
2022-03-01 15:00:26 +0100

    better script

--- scripts/get_paper.py
+++ scripts/get_paper.py
@@ -1,16 +1,37 @@
 import requests
+import sys
 
-version = "1.18.1"
-destPath = "../server/paper.jar"
 
-base = "https://papermc.io/api/v2/projects/paper/versions"
-r = requests.get(f"{base}/{version}")
+def get_paper(version, destination):
+    print(f"Trying to fetch {version} into {destination}")
 
-build = max(r.json()['builds'])
-r = requests.get(f"{base}/{version}/builds/{build}")
+    base = "https://papermc.io/api/v2/projects/paper/versions"
+    r = requests.get(f"{base}/{version}")
+    if not r.ok:
+        print(f"Cannot find version {version}, aborting.")
+        return
 
-download = r.json()['downloads']['application']['name']
-content = requests.get(f"{base}/{version}/builds/{build}/downloads/{download}").content
+    build = max(r.json()['builds'])
+    r = requests.get(f"{base}/{version}/builds/{build}")
 
-with open(destPath, "wb") as file:
-    file.write(content)
+    download = r.json()['downloads']['application']['name']
+    content = requests.get(f"{base}/{version}/builds/{build}/downloads/{download}").content
+
+    with open(destination, "wb") as jarFile:
+        jarFile.write(content)
+
+
+def main(argv):
+    if len(argv) == 1:
+        print("No version defined!")
+        return
+    version = argv[1]
+    if len(argv) == 2:
+        destination = "paper.jar"
+    else:
+        destination = argv[2]
+    get_paper(version, destination)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
